a0a281e725bea2fc20b718cc589126ae649bb1f8
Ivan Zinovyev <zinovyev-ivan@yandex.ru>
2018-01-25 14:24:22 +0300

    Add swiftlint config

--- xcode/build_phases/swiftlint.sh
+++ xcode/build_phases/swiftlint.sh
@@ -1,1 +1,1 @@
-${PODS_ROOT}/SwiftLint/swiftlint autocorrect --path ${PROJECT_NAME} && ${PODS_ROOT}/SwiftLint/swiftlint --path ${PROJECT_NAME} --config ${PROJECT_DIR}/code-quality/.swiftlint.yml
+${PODS_ROOT}/SwiftLint/swiftlint autocorrect --path ${PROJECT_NAME} && ${PODS_ROOT}/SwiftLint/swiftlint --path ${PROJECT_NAME} --config ${PROJECT_DIR}/build-scripts/xcode/.swiftlint.yml
