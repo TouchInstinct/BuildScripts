c097c93a53898f8c2c32a187cc3f05d9ddbc1db0
Ivan Smolin <ivan.smolin@touchin.ru>
2018-03-20 17:38:10 +0300

    add path to sources argument

--- xcode/build_phases/copy_paste_detection.sh
+++ xcode/build_phases/copy_paste_detection.sh
@@ -1,6 +1,6 @@
 # running CPD
-echo ${EXECUTABLE_NAME}
-pmd cpd --files ${EXECUTABLE_NAME} --exclude ${EXECUTABLE_NAME}/Generated --minimum-tokens 50 --language swift --encoding UTF-8 --format net.sourceforge.pmd.cpd.XMLRenderer > cpd-output.xml --failOnViolation true
+SOURCES_DIR=${1:-${EXECUTABLE_NAME}} # first argument or EXECUTABLE_NAME
+pmd cpd --files ${SOURCES_DIR} --exclude ${EXECUTABLE_NAME}/Generated --minimum-tokens 50 --language swift --encoding UTF-8 --format net.sourceforge.pmd.cpd.XMLRenderer > cpd-output.xml --failOnViolation true
 
 # running script
 php ./build-scripts/xcode/aux_scripts/cpd_script.php -cpd-xml cpd-output.xml
