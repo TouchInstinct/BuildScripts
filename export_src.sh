f50c1fef04cc1cc6e2aa9944601a53e0d6099a05
Ivan Smolin <ivan.smolin@touchin.ru>
2019-06-14 11:41:53 +0300

    export android src as well

--- export_src.sh
+++ export_src.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
-PROJECT_DIR=$1
-PLATFORM=$2
+PROJECT_NAME=$1
+SRC_FOLDER_NAME=${PROJECT_NAME}-src-$(date +%F)
+SRC_DIR=/tmp/${SRC_FOLDER_NAME}
 
-cd /tmp/
-git clone --recurse-submodules -j8 git@github.com:TouchInstinct/${PROJECT_DIR}-${PLATFORM}.git --branch develop
-cd ${PROJECT_DIR}-${PLATFORM}
+clone_platform() {
+    PROJECT_DIR=$1
+    PLATFORM=$2
+
+    git clone --recurse-submodules -j8 git@github.com:TouchInstinct/${PROJECT_DIR}-${PLATFORM}.git --branch master
+}
+
+mkdir -p ${SRC_DIR}
+cd ${SRC_DIR}
+
+clone_platform ${PROJECT_NAME} ios
+clone_platform ${PROJECT_NAME} android
+
 find . -name ".git*" -print0 | xargs -0 rm -rf
-zip -r ${PROJECT_DIR}-${PLATFORM}-src-$(date +%F).zip .
+zip -r ${SRC_FOLDER_NAME}.zip .
 
 open .
