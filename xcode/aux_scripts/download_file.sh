618547c0806a6bff58cdca791f8ef3983c1a26b7
Maxim Sorokin <maxim.sorokin@touchin.ru>
2020-01-15 16:42:58 +0300

    added folder and file to delete capabilities for download_file.sh

--- xcode/aux_scripts/download_file.sh
+++ xcode/aux_scripts/download_file.sh
@@ -1,15 +1,8 @@
 file_name=$1
 file_link=$2
-folder=$3
+readonly folder=${3:-"Downloads"}
 flag_of_delete=$4
-
-key_of_delete="--remove-cached"
-default_folder="Downloads"
-
-if ! [ -n "$folder" ]; then
-    folder="${default_folder}"
-fi
-
+readonly key_of_delete="--remove-cached"
 file_path="./${folder}/${file_name}"
 
 if [[ ${flag_of_delete} = ${key_of_delete} ]]; then
@@ -18,7 +11,7 @@ fi
 
 # make folder if not exist
 if ! [ -e ${folder} ]; then
-    mkdir ${folder}
+    mkdir -p ${folder}
 fi
 
 # download file if not downloaded
