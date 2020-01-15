94c179f6662c521458f00f35db94867ea3b4776d
Maxim Sorokin <maxim.sorokin@touchin.ru>
2020-01-15 13:52:43 +0300

    added folder and file to delete capabilities for download_file.sh

--- xcode/aux_scripts/download_file.sh
+++ xcode/aux_scripts/download_file.sh
@@ -1,8 +1,20 @@
 file_name=$1
 file_link=$2
+folder_to_delete=$3
+file_to_delete=$4
 
 folder="Downloads"
 file_path="./${folder}/${file_name}"
+
+# remove folder and all files it
+if [ -e ${folder_to_delete} ]; then
+    rm -rf ${folder_to_delete}
+fi
+
+# remove some file
+if [ -e ${file_to_delete} ]; then
+    rm ${file_to_delete}
+fi
 
 # make folder if not exist
 if ! [ -e ${folder} ]; then
