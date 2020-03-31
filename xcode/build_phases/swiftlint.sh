0291efe8c9845e5352c2223dbf09f108645e9dba
Ivan Smolin <ivan.smolin@touchin.ru>
2020-03-31 12:29:45 +0300

    use $TARGET_NAME instead of $PROJECT_NAME as default folder path for linting

--- xcode/build_phases/swiftlint.sh
+++ xcode/build_phases/swiftlint.sh
@@ -1,2 +1,2 @@
-SOURCES_DIR=${1:-${PROJECT_NAME}} # first argument or PROJECT_NAME
+SOURCES_DIR=${1:-${TARGET_NAME}} # first argument or TARGET_NAME
 ${PODS_ROOT}/SwiftLint/swiftlint autocorrect --path ${SOURCES_DIR} --config ${PROJECT_DIR}/build-scripts/xcode/.swiftlint.yml && ${PODS_ROOT}/SwiftLint/swiftlint --path ${SOURCES_DIR} --config ${PROJECT_DIR}/build-scripts/xcode/.swiftlint.yml
