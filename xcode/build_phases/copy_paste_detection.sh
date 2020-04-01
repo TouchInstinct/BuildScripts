a8284d136eaa67724411b774cfd833bb67da93e5
Ivan Smolin <ivan.smolin@touchin.ru>
2020-04-01 15:16:08 +0300

    make cpd paths relative, so developers won't rewrite entire file

--- xcode/build_phases/copy_paste_detection.sh
+++ xcode/build_phases/copy_paste_detection.sh
@@ -9,7 +9,11 @@ if which pmd >/dev/null; then
     pmd cpd --files ${SOURCES_DIR} --exclude ${FILES_TO_EXCLUDE} --minimum-tokens 50 --language swift --encoding UTF-8 --format net.sourceforge.pmd.cpd.XMLRenderer > ${REPORTS_DIR}/cpd-output.xml --failOnViolation true
 
     # running script
-    php ./build-scripts/xcode/aux_scripts/cpd_script.php ${REPORTS_DIR}/cpd-output.xml | tee ${REPORTS_DIR}/CPDLog.txt
+    readonly WARNINGS=$(php ./build-scripts/xcode/aux_scripts/cpd_script.php ${REPORTS_DIR}/cpd-output.xml)
+    # Make paths relative to SOURCES_DIR, so different developers won't rewrite entire file
+    readonly SED_REPLACEMENT_STRING=$(echo ${SOURCES_DIR} | sed "s/\//\\\\\//g")
+    echo ${WARNINGS} | sed "s/${SED_REPLACEMENT_STRING}//" > ${REPORTS_DIR}/CPDLog.txt
+    echo ${WARNINGS}
 else
     echo "warning: pmd not installed, install using 'brew install pmd'"
     exit 1
