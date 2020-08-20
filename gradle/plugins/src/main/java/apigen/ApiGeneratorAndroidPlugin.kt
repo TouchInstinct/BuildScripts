b3bff241398f0545fe1870b62371594c495e5825
Maxim Bachinsky <maxim.bachinsky@touchin.ru>
2020-08-20 15:22:41 +0300

    fixed problem with source sets for java code

--- gradle/plugins/src/main/java/apigen/ApiGeneratorAndroidPlugin.kt
+++ gradle/plugins/src/main/java/apigen/ApiGeneratorAndroidPlugin.kt
@@ -3,6 +3,7 @@ package apigen
 import com.android.build.gradle.AppExtension
 import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Project
+import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.kotlin.dsl.findByType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -31,6 +32,12 @@ class ApiGeneratorAndroidPlugin : ApiGeneratorPlugin() {
                 }
                 tasks
                         .filterIsInstance(KotlinCompile::class.java)
+                        .forEach {
+                            it.source(outputDir)
+                        }
+
+                tasks
+                        .filterIsInstance<JavaCompile>()
                         .forEach {
                             it.source(outputDir)
                         }
