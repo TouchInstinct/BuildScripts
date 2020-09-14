94f1d45c7f59ff435e9f55adb3839376e6c886ae
Vlad <Suhomlinov.dev@gmail.com>
2020-09-14 22:11:59 +0300

    Code correction

--- xcode/build_phases/localization_check.sh
+++ xcode/build_phases/localization_check.sh
@@ -1,6 +1,6 @@
 # source: https://github.com/iKenndac/verify-string-files
 
-SOURCES_DIR=${1:-${TARGET_NAME}} # first argument or TARGET_NAME
+readonly SOURCES_DIR=${1:-${PROJECT_DIR}} # first argument or PROJECT_DIR
 
 if [ "${CONFIGURATION}" = "DEBUG" ]; then
 	${SOURCES_DIR}/build-scripts/xcode/build_phases/common/localization_check -master ${SOURCES_DIR}/Resources/Base.lproj/Localizable.strings -warning-level warning
