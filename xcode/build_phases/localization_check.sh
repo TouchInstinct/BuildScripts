80cc86b4f817d971744356572c015b426fb5e731
Vlad <Suhomlinov.dev@gmail.com>
2020-09-14 22:14:01 +0300

    Code correction

--- xcode/build_phases/localization_check.sh
+++ xcode/build_phases/localization_check.sh
@@ -3,7 +3,7 @@
 readonly SOURCES_DIR=${1:-${PROJECT_DIR}} # first argument or PROJECT_DIR
 
 if [ "${CONFIGURATION}" = "DEBUG" ]; then
-	${SOURCES_DIR}/build-scripts/xcode/build_phases/common/localization_check -master ${SOURCES_DIR}/Resources/Base.lproj/Localizable.strings -warning-level warning
+	${SOURCES_DIR}/build-scripts/xcode/build_phases/common/localization_check -master ${SOURCES_DIR}/Resources/Localization/Base.lproj/Localizable.strings -warning-level warning
 else
-	${SOURCES_DIR}/build-scripts/xcode/build_phases/common/localization_check -master ${SOURCES_DIR}/Resources/Base.lproj/Localizable.strings
+	${SOURCES_DIR}/build-scripts/xcode/build_phases/common/localization_check -master ${SOURCES_DIR}/Resources/Localization/Base.lproj/Localizable.strings
 fi
