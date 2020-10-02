ef6d0eb3ccccd524669a199a710d6c409ec3ad23
Vlad <Suhomlinov.dev@gmail.com>
2020-10-02 11:28:06 +0300

    Code correction

--- xcode/build_phases/unused_resources.sh
+++ xcode/build_phases/unused_resources.sh
@@ -1,7 +1,7 @@
-readonly SOURCES_DIR=${1:-${PROJECT_DIR}} # first argument or PROJECT_DIR
+readonly SOURCES_DIR=${1:-${PROJECT_DIR}/${PRODUCT_NAME}} # first argument or PROJECT_DIR
 readonly UNUSED_RESOURCES_SCRIPT=${2:-${PROJECT_DIR}/build-scripts/xcode/build_phases/common/unused_resources} # second argument set check script path
 readonly REPORTS_DIR=${PROJECT_DIR}/code-quality-reports
-readonly FILES_TO_EXCLUDE=`find ${SOURCES_DIR} -type d -name Localization -or -name Generated -or -name Carthage -or -name Pods | paste -sd " " -`
+readonly FILES_TO_EXCLUDE=`find ${SOURCES_DIR} -type d -name Localization -or -name Generated | paste -sd " " -`
 
 mkdir ${REPORTS_DIR}
 
