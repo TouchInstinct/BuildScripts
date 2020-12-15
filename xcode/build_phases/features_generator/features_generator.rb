e7574decbc69dd7b9be5fd35aa18caa3c8ed92c1
Vlad <Suhomlinov.dev@gmail.com>
2020-12-15 11:05:35 +0300

    Add check for nil

--- xcode/build_phases/features_generator/features_generator.rb
+++ xcode/build_phases/features_generator/features_generator.rb
@@ -34,6 +34,10 @@ end
 
 build_settings_features_list = Managers::FileManager.load_from_file_YAML(build_settings_file_path)["features"]
 
+if build_settings_features_list.nil? or build_settings_features_list.empty?
+	raise "There are no features in " + build_settings_file_path
+end
+
 utils = FeatureUtils.new(build_settings_features_list)
 rendered_enum = utils.render(features_enum_template).strip
 
