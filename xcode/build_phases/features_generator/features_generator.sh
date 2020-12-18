4777c0c5977783867282157371f304df8e8920b1
Vlad <Suhomlinov.dev@gmail.com>
2020-12-18 17:55:27 +0300

    Fix PR

--- xcode/build_phases/features_generator/features_generator.sh
+++ xcode/build_phases/features_generator/features_generator.sh
@@ -3,7 +3,8 @@ readonly BUILD_SETTINGS_FILE_PATH=${1:-${PROJECT_DIR}/common/build_settings.yaml
 readonly FEATURES_ENUM_FILE_PATH=${2:-${PROJECT_DIR}/${PRODUCT_NAME}/Resources/Features/FeatureToggle.swift}
 
 # Features enunm generator script
-readonly GENERATOR_SCRIPT=${PROJECT_DIR}/build-scripts/xcode/build_phases/features_generator/features_generator.rb
+readonly CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+readonly GENERATOR_SCRIPT=${CURRENT_DIR}/features_generator.rb
 
 if ! [ -e ${BUILD_SETTINGS_FILE_PATH} ]; then
 	echo "File ${BUILD_SETTINGS_FILE_PATH} does not exist. Add this file and try again."
