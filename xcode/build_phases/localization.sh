cf2aa3f7ee10030f140f8aa5b3743b66e0bf5799
Ivan Smolin <ivan.smolin@touchin.ru>
2021-07-21 17:17:22 +0300

    localization script refactoring

--- xcode/build_phases/localization.sh
+++ xcode/build_phases/localization.sh
@@ -1,16 +1,36 @@
-LOCALIZATION_PATH="${PRODUCT_NAME}/Resources/Localization"
-#first argument set strings folder path
+#!/bin/sh
+
+# Description:
+#   Generates Localizeable.strings and String+Localization.swift files.
+#
+# Parameters:
+#   $1 - path to strings folder containing json files.
+#   $2 - path to Localization folder (output).
+#   $3 - Bundle for localization. Default is `.main`.
+#
+# Required environment variables:
+#   SCRIPT_DIR - directory of current script.
+#
+# Optional environment variables:
+#   PRODUCT_NAME - product name to produce path to localization folder (output).
+#
+# Examples of usage:
+#   . localization.sh
+#   . localization.sh common/strings Resources/Localization/ .main
+#
+
 STRINGS_FOLDER=${1:-"common/strings"}
+LOCALIZATION_PATH=${2:-"${PRODUCT_NAME}/Resources/Localization/"}
+BUNDLE=${3:-".main"}
 
 if ! [ -e ${LOCALIZATION_PATH} ]; then
-	echo "${PROJECT_DIR}/${LOCALIZATION_PATH} path does not exist. Add these folders and try again."
+	echo "${LOCALIZATION_PATH} path does not exist. Add these folders and try again."
 	exit 1
 fi
 
-if ! [ -e "${PROJECT_DIR}/${STRINGS_FOLDER}" ]; then
-	echo "${PROJECT_DIR}/${STRINGS_FOLDER} path does not exist. Submodule with strings should be named common and contain strings folder."
+if ! [ -e "${STRINGS_FOLDER}" ]; then
+	echo "${STRINGS_FOLDER} path does not exist. Submodule with strings should be named common and contain strings folder."
 	exit 1
 fi
 
-#second argument set strings script path
-php ${2:-build-scripts/xcode/aux_scripts/import_strings.php} ${PRODUCT_NAME} ${STRINGS_FOLDER}
+php ${SCRIPT_DIR}/../aux_scripts/import_strings.php ${LOCALIZATION_PATH} ${STRINGS_FOLDER} ${BUNDLE}
